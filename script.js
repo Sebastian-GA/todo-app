a3505a916005eb7b135bacc312baa96c165a3352
Sebastian GA <isebastian.ga@gmail.com>
2023-03-19 00:41:11 -0500

    done and trash buttons

--- script.js
+++ script.js
@@ -7,14 +7,34 @@ const createTask = (event) => {
     input.value = ""; // clear the input field
     if (value === "") return;
 
+    // create task
     const task = document.createElement("li");
     task.classList.add("task-card");
     task.setAttribute("data-task", "");
-
-    const content = `<button class="task-btn task-done-btn"><i class="far fa-check-square"></i></button>
-                    <span class="task-name">${value}</span>
-                    <button class="task-btn task-trash-btn"><i class="fas fa-trash-alt"></i></button>`;
-    task.innerHTML = content;
+    // create checkbox
+    const checkbox = document.createElement("button");
+    checkbox.classList.add("task-btn", "task-done-btn");
+    checkbox.setAttribute("data-task-done-btn", "");
+    checkbox.innerHTML = `<i class="far fa-check-square"></i>`;
+    checkbox.addEventListener("click", () => {
+        task.classList.toggle("task-done");  // toggle done status
+    });
+    task.appendChild(checkbox);
+    // create task name
+    const taskName = document.createElement("span");
+    taskName.classList.add("task-name");
+    taskName.setAttribute("data-task-name", "");
+    taskName.innerText = value;
+    task.appendChild(taskName);
+    // create trash button
+    const trashBtn = document.createElement("button");
+    trashBtn.classList.add("task-btn", "task-trash-btn");
+    trashBtn.setAttribute("data-task-trash-btn", "");
+    trashBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+    trashBtn.addEventListener("click", () => {
+        task.remove();  // remove the task
+    });
+    task.appendChild(trashBtn);
 
     document.querySelector("[data-tasks-list]").appendChild(task);
 };
